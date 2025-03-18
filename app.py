da64dfef8174f94c6126d31d7ada4a561c9a99c6
indra8959 <70633470+indra8959@users.noreply.github.com>
2025-03-18 18:22:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def delete_user():
         return jsonify({"error": "User not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8800,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
